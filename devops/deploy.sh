8c683812cbcb2125462cba3add66d8ed3887d49f
D.V.D <entropyhacker@gmail.com>
2021-11-28 14:14:09 +0200

    fix: prod deployment config

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 export PROJECT_ID="$(basename $(pwd) | tr [A-Z] [a-z])"
-./db_migrate.sh
+#./devops/db_migrate.sh
 echo "Updating services..."
-dotenv docker stack deploy --with-registry-auth -c docker-compose.yml $PROJECT_ID
+dotenv docker stack deploy --with-registry-auth -c docker-compose.yml -c docker-compose.swarm.yml $PROJECT_ID
 sleep 5 && docker stack services $PROJECT_ID
